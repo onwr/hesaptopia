8b73162e2a8cd08685c19c0c9285a8fe6adffecd
onwr <onurkurkaya@gmail.com>
2024-10-03 07:36:04 +0300

    sa

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        siyah: "#1A1B1F",
+        gray: "#24252A",
+        primary: "#3572EF",
+        customGreen: "#04CF00",
+        graylight: "#D9D9D9",
+        greenhafif: "#2DFF354A",
+        bluelight: "#0094FF",
+      },
+    },
+  },
+  plugins: [],
+};
